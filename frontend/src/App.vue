af4e70058b0e236b7a5ce883b9a40624a74fb74e
kkWin <2019274001@email.szu.edu.cn>
2022-06-22 23:05:29 +0800

    fe commit test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <t-button :click="onClick()"> 按钮</t-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+
+// import './custom.scss'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+      this.$router
+        .resolve({
+          name: "HelloWorld",
+          path: "/queryPage",
+          params: {
+            msg: "Hello World",
+          },
+        })
+        .then(({ component }) => {
+          console.log(component);
+        });
+    },
+  },
+};
 </script>
 
 <style>
---
+++ frontend/src/queryPage.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <t-button>11</t-button>
+    </div>
+</template>
+
+
+
